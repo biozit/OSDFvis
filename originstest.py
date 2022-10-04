98261cd652bea2ebcf7e5d5c60dd50cf0f2d9e56
Fabio Andrijauskas <biozit@gmail.com>
2022-10-04 14:07:13 -0700

    f2

--- originstest.py
+++ originstest.py
@@ -84,7 +84,7 @@ for origin in lorigins:
                 else:
                       path = origin.split(" ")
                       path = path[0] + "/" + path[1]
-                      dd = "/opt/xrdcopy -f " +path + " ." 
+                      dd = "xrdcopy -f " +path + " ." 
                       ex = executeCommandBD(dd,1)
                       resp = getSpeed(ex,2)
                 dbc = " "
