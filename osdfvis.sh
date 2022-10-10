e76be78d618fde8a81bce2ee83047de09bc21e91
Fabio Andrijauskas <biozit@gmail.com>
2022-10-10 12:18:49 -0700

    f1

--- osdfvis.sh
+++ osdfvis.sh
@@ -3,6 +3,7 @@ FILE=/usr/bin/git
 if [ ! -f "$FILE" ]; then
     yum -y install git >>/var/log/vis 2>&1
     pip3 install numpy >>/var/log/vis 2>&1
+    pip3 install pythonping >>/var/log/vis 2>&1
     cd /opt >>/var/log/vis 2>&1
     /usr/bin/git clone https://github.com/biozit/OSDFvis.git >>/var/log/vis 2>&1
 fi
