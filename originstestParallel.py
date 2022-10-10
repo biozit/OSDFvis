ef83eeb91a74b599585a2309db7994aa5038ba3a
Fabio Andrijauskas <biozit@gmail.com>
2022-10-10 11:58:56 -0700

    Update originstestParallel.py

--- originstestParallel.py
+++ originstestParallel.py
@@ -9,7 +9,7 @@ import time
 from XRootD import client
 import numpy as np
 from influxdb import InfluxDBClient
-from datetime import date
+from datetime import datetime
 
 file_lorigins = open("/opt/OSDFvis/origins.txt", "r")
 lorigins = lines = file_lorigins.read().splitlines()
